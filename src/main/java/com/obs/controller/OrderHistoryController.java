a9a4c032f933cfdd0a46c2e61d207b807ffd324c
ragavalliommi <82811467+ragavalliommi@users.noreply.github.com>
2022-11-29 13:50:11 -0600

    Incorporating order detail in history

--- src/main/java/com/obs/controller/OrderHistoryController.java
+++ src/main/java/com/obs/controller/OrderHistoryController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.obs.dao.DbConnector;
-
+import com.obs.model.Cart;
 import com.obs.model.Order;
 
 
@@ -66,7 +66,36 @@ public class OrderHistoryController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		// TODO Auto-generated method stub
+				response.getWriter().append("Served at: ").append(request.getContextPath());
+				// set credentials
+				if (request.getParameter("userID") != null) {
+					request.setAttribute("userID", request.getParameter("userID"));
+				}else {
+					request.setAttribute("userID", null);
+					
+				}
+				
+				if (request.getParameter("userName") != null) {
+					
+					request.setAttribute("userName", request.getParameter("userName"));
+				}else {
+					request.setAttribute("userName", null);
+					
+				}
+				String userID = request.getParameter("userID");
+				String orderID = request.getParameter("orderID");
+				Cart cart = null;
+				try {
+					cart = orderHistoryDao.getCart(userID, orderID);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				request.setAttribute("cart_data", cart.getCartItems());
+				RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/views/OrderDetail.jsp");
+				requestDispatcher.forward(request, response);
+
 	}
 	
 
