fbe81945ca1619a826f1b720853460e7679f65db
Sneha Siripurapu <snehaguptha03@gmail.com>
2022-11-30 18:07:51 -0600

    Cart Controller tests added

---
+++ src/test/java/com/obs/controller/CartControllerTest.java
@@ -0,0 +1,90 @@
+package com.obs.controller;
+
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+
+public class CartControllerTest{
+	
+	@Mock
+	HttpServletRequest request;
+	
+	@Mock
+	HttpServletResponse response;
+	
+	@Mock
+	RequestDispatcher rd;
+	
+	CartController cart = new CartController();
+	
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void getCartControllerTest() throws Exception {
+		when(request.getServletPath()).thenReturn("/cart");
+		when(request.getParameter("userID")).thenReturn("4");
+		when(request.getParameter("userName")).thenReturn("sneha");
+		when(request.getParameter("itemID")).thenReturn("11");
+		when(request.getParameter("quantity")).thenReturn("1");
+		when(request.getRequestDispatcher("/WEB-INF/views/Cart.jsp")).thenReturn(rd);
+		
+		cart.doGet(request, response);
+		verify(rd).forward(request, response);
+	}
+	
+	@Test
+	public void getCartControllerTestNullUserName() throws Exception {
+		when(request.getServletPath()).thenReturn("/cart");
+		when(request.getParameter("userID")).thenReturn("4");
+		when(request.getParameter("userName")).thenReturn(null);
+		when(request.getParameter("itemID")).thenReturn("11");
+		when(request.getParameter("quantity")).thenReturn("1");
+		when(request.getRequestDispatcher("/WEB-INF/views/Cart.jsp")).thenReturn(rd);
+		
+		cart.doGet(request, response);
+		verify(rd).forward(request, response);
+	}
+	
+	@Test
+	public void postCartControllerTest() throws Exception {
+		when(request.getServletPath()).thenReturn("/cart");
+		when(request.getParameter("userID")).thenReturn("4");
+		when(request.getParameter("userName")).thenReturn("sneha");
+		when(request.getParameter("bakeryItemID")).thenReturn("11");
+		when(request.getParameter("quantity")).thenReturn("1");
+		when(request.getRequestDispatcher("/WEB-INF/views/Cart.jsp")).thenReturn(rd);
+		
+		cart.doPost(request, response);
+		verify(rd).forward(request, response);
+	}
+	
+	@Test
+	public void postCartControllerTestNullUserName() throws Exception {
+		when(request.getServletPath()).thenReturn("/cart");
+		when(request.getParameter("userID")).thenReturn("4");
+		when(request.getParameter("userName")).thenReturn(null);
+		when(request.getParameter("bakeryItemID")).thenReturn("11");
+		when(request.getParameter("quantity")).thenReturn("1");
+		when(request.getRequestDispatcher("/WEB-INF/views/Cart.jsp")).thenReturn(rd);
+		
+		cart.doPost(request, response);
+		verify(rd).forward(request, response);
+	}
+
+}
